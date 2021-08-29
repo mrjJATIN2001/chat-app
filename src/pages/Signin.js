36511838007e3cdd67712e17cfebeb17232bcc1a
mrjJATIN2001 <jatintak565@gmail.com>
2021-08-29 17:59:48 +0530

    added sign in page and sign in methods

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,8 +1,60 @@
+/* eslint-disable no-console */
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Panel, Row, Col, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const Signin = () => {
-  return <div>Sign In</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  const onPhoneSignIn = () => {
+    signInWithProvider(new firebase.auth.PhoneAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to chat</h2>
+                <p>Progressive chat platform</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+                <Button block color="blue" onClick={onPhoneSignIn}>
+                  <Icon icon="phone" /> Continue with Phone
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Signin;
