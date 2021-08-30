f3a2f5a5d9d4d8cdd09ab6d3afc26cc2838f3437
mrjJATIN2001 <jatintak565@gmail.com>
2021-08-30 12:29:02 +0530

    uploaded user avatar

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyAi9b4rB6VK7MLcUOWoxIkrLyb79VgQ-38',
@@ -14,3 +15,4 @@ const config = {
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
